12a2309c8e98ddeb61fe73bf5ee77796905e01c8
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-06-25 22:53:02 +0530

    initialize tic-tac-toe board with empty spaces

---
+++ first.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args) {
+        char[][] board = new char [3][3];
+        for(int row =0; row < board.length; row++){
+            for(int col = 0; col < board[row].length; col++){
+                board[row][col] = ' ';
+            }
+        }
+    }
+}
