6cdf7c83e41afcf30cb6d011cbdfdd01d1892d12
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-06-29 20:11:01 +0530

    feat: implement win condition check in printBoard method

--- first.java
+++ first.java
@@ -48,5 +48,17 @@ class Main{
                 System.out.println("-----");
             }
         }
+        if (
+            board[0][0] == player && board[0][1] == player && board[0][2] == player ||
+            board[1][0] == player && board[1][1] == player && board[1][2] == player ||
+            board[2][0] == player && board[2][1] == player && board[2][2] == player ||
+            board[0][0] == player && board[1][0] == player && board[2][0] == player ||
+            board[0][1] == player && board[1][1] == player && board[2][1] == player ||
+            board[0][2] == player && board[1][2] == player && board[2][2] == player ||
+            board[0][0] == player && board[1][1] == player && board[2][2] == player ||
+            board[0][2] == player && board[1][1] == player && board[2][0] == player
+        ) {
+            
+        }
     }
 }
